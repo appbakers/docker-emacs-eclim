23b1be3a6d0c0f8a5ad88def337e60ce2ce49d4b
appbakers <greeny100@gmail.com>
2017-07-24 08:57:31 +0900

    *auto

--- emacs.d/customizations/setup-eclim.el
+++ emacs.d/customizations/setup-eclim.el
@@ -1,28 +1,35 @@
+(require 'cl)
 (require 'eclim)
 
 (setq eclimd-autostart t)
+(global-eclim-mode)
 (custom-set-variables
-	'(eclim-eclipse-dirs '("~/eclipse"))
-	'(eclim-executable "~/eclipse/eclim"))
+  '(eclim-eclipse-dirs '("/home/docker/eclipse"))
+  '(eclim-executable "/home/docker/eclipse/eclim")
+  '(eclimd-default-workspace "/home/docker/workspace"))
 
+
+;; Display compilation error messages in the echo area
+(setq help-at-pt-display-when-idle t)
+(setq help-at-pt-timer-delay 0.1)
+(help-at-pt-set-timer)
+
+
+;; auto-complete
+(require 'auto-complete-config)
+(ac-config-default)
+(require 'ac-emacs-eclim)
+(ac-emacs-eclim-config)
+(ac-set-trigger-key "TAB")
+(define-key ac-complete-mode-map (kbd "C-n") 'ac-next)
+(define-key ac-complete-mode-map (kbd "C-p") 'ac-previous)
 (defun my-java-mode-hook ()
 	(eclim-mode t))
 (add-hook 'java-mode-hook 'my-java-mode-hook)
 
 
-(setq help-at-pt-display-when-idle t)
-(setq help-at-pt-timer-delay 0.1)
-(help-at-pt-set-timer)
-
-(require 'auto-complete-config)
-(ac-config-default)
-
-(require 'ac-emacs-eclim)
-(ac-emacs-eclim-config)
-
-
+;; company
 (require 'company)
 (require 'company-emacs-eclim)
 (company-emacs-eclim-setup)
 (global-company-mode t)
-
